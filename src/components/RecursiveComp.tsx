ab2553bc8645d1369744a4a69f7afd99340f3374
NikhilAndola <nikhil@coredge.io>
2024-11-23 09:14:19 +0530

    1st commit: designing the recursive component

--- src/components/RecursiveComp.tsx
+++ src/components/RecursiveComp.tsx
@@ -1,26 +1,25 @@
-import { useState } from 'react';
 import * as React from 'react';
 import './App.css';
 
 // import { data } from '../mocks/mock.json';
+const stateData = [
+  {
+    id: 1,
+    name: 'Item 1',
+    children: [
+      { id: 2, name: 'Item 1.1', children: [] },
+      { id: 3, name: 'Item 1.2', children: [] },
+    ],
+  },
+  { id: 4, name: 'Item 2' },
+];
 
 function RecursiveComp() {
-  const [globalState, setGlobalState] = React.useState(null);
+  const [globalState, setGlobalState] = React.useState<null | typeof stateData>(null);
 
   console.log('globalState', globalState);
 
   React.useEffect(() => {
-    const stateData = [
-      {
-        id: 1,
-        name: 'Item 1',
-        children: [
-          { id: 2, name: 'Item 1.1' },
-          { id: 3, name: 'Item 1.2' },
-        ],
-      },
-      { id: 4, name: 'Item 2' },
-    ];
     setGlobalState(stateData);
   }, []);
 
